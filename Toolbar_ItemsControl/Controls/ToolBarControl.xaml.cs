64a666b37ee04f3a30af6d65b55d80c69a6952d4
Artur <arturdrm@hotmail.com>
2022-03-05 19:11:04 -0300

    ADDED: RoutedEvents

--- Toolbar_ItemsControl/Controls/ToolBarControl.xaml.cs
+++ Toolbar_ItemsControl/Controls/ToolBarControl.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -25,32 +26,112 @@ namespace Toolbar_ItemsControl.Controls
     {
         public partial class ToolBarModel
         {
-            private int Index { get; set; }
+            /// <summary>
+            /// Index que indica com qual item estou interagindo
+            /// </summary>
+            public int Index { get; set; }
         }
         public ToolBarControl()
         {
             InitializeComponent();
+            ToolBar.Loaded += ToolBar_Loaded;
+
         }
 
-
-
-
-        public IEnumerable<ToolBarModel> SourceList
+        private void ToolBar_Loaded(object sender, RoutedEventArgs e)
         {
-            get { return (IEnumerable<ToolBarModel>)GetValue(SourceListProperty); }
-            set { SetValue(SourceListProperty, value); }
+            InitList();
         }
 
-        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty SourceListProperty =
-    DependencyProperty.Register("SourceList", typeof(IEnumerable<ToolBarModel>), typeof(UserControl), new PropertyMetadata(null));
+        private void InitList()
+        {
+            ItemsSource = new ObservableCollection<ToolBarModel>();
+            for (int i = 0; i < NumItems; i++)
+            {
+                ItemsSource.Add(new ToolBarModel
+                {
+                    Index = i
+                });
+            }
+            NotifyPropertyChanged(nameof(ItemsSource));
+        }
+        private void Up_Click(object sender, RoutedEventArgs e)
+        {
+            var obj = sender as Button;
+            var index = (int)obj.Tag;
+            RaiseEvent(new RoutedPropertyChangedEventArgs<int>(0, index, ButtonUpEvent));
+        }
+
+        private void Down_Click(object sender, RoutedEventArgs e)
+        {
+            var obj = sender as Button;
+            var index = (int)obj.Tag;
+            RaiseEvent(new RoutedPropertyChangedEventArgs<int>(0, index, ButtonDownEvent));
+        }
+
+        private void IsVisivle_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public ObservableCollection<ToolBarModel> ItemsSource { get; set; }
 
 
+        #region RoutedEvents
+        public event RoutedPropertyChangedEventHandler<int>  UpClick
+        {
+            add
+            {
+                AddHandler(ButtonUpEvent, value);
+            }
+            remove
+            {
+                RemoveHandler(ButtonUpEvent, value);
+            }
+        }
+        public static RoutedEvent ButtonUpEvent = EventManager.RegisterRoutedEvent(nameof(UpClick),
+          RoutingStrategy.Bubble, typeof(RoutedPropertyChangedEventHandler<int>), typeof(ToolBarControl) );
 
+
+        public event RoutedPropertyChangedEventHandler<int>  DownClick
+        {
+            add
+            {
+                AddHandler(ButtonDownEvent, value);
+            }
+            remove
+            {
+                RemoveHandler(ButtonDownEvent, value);
+            }
+        }
+        public static RoutedEvent ButtonDownEvent = EventManager.RegisterRoutedEvent(nameof(DownClick),
+          RoutingStrategy.Bubble, typeof(RoutedPropertyChangedEventHandler<int>), typeof(ToolBarControl) );
+        #endregion
+
+
+        #region DependencyProperty
+        /// <summary>
+        /// Quantidade de itens para serem mostrados
+        /// </summary>
+        public int NumItems
+        {
+            get { return (int)GetValue(NumItemsProperty); }
+            set { SetValue(NumItemsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for NumItems.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NumItemsProperty =
+    DependencyProperty.Register("NumItems", typeof(int), typeof(ToolBarControl), new PropertyMetadata(1));
+        #endregion
+
+        #region NotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged([CallerMemberName] string propertyname = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
         }
+        #endregion
+
+
     }
 }
--- Toolbar_ItemsControl/MainWindow.xaml.cs
+++ Toolbar_ItemsControl/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +20,62 @@ namespace Toolbar_ItemsControl
     /// <summary>
     /// Interação lógica para MainWindow.xam
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            Main.Loaded += Main_Loaded;
+        }
+
+        private void Main_Loaded(object sender, RoutedEventArgs e)
+        {
+            LeftValue = 0;
+            NotifyPropertyChanged(nameof(LeftValue));
+
+            RigthValue = 0;
+            NotifyPropertyChanged(nameof(RigthValue));
+        }
+
+        public int LeftValue { get; set; }
+        public int RigthValue { get; set; }
+
+        
+
+        private void ToolBarControl_UpClick(object sender, RoutedPropertyChangedEventArgs<int> e)
+        {
+            var index = e.NewValue;
+            if (index == 0)
+            {
+                LeftValue = LeftValue + 10;
+                NotifyPropertyChanged(nameof(LeftValue));
+            }
+            else if (index == 1)
+            {
+                RigthValue = RigthValue + 10;
+                NotifyPropertyChanged(nameof(RigthValue));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyname = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+
+        private void ToolBarControl_DownClick(object sender, RoutedPropertyChangedEventArgs<int> e)
+        {
+            var index = e.NewValue;
+            if (index == 0)
+            {
+                LeftValue = LeftValue - 10;
+                NotifyPropertyChanged(nameof(LeftValue));
+            }
+            else if (index == 1)
+            {
+                RigthValue = RigthValue - 10;
+                NotifyPropertyChanged(nameof(RigthValue));
+            }
         }
     }
 }
